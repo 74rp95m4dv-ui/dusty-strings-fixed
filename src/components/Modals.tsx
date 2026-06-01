9819763f0efcf8ce1d54cee34b06198463db87be
74rp95m4dv-ui <282754488+74rp95m4dv-ui@users.noreply.github.com>
2026-05-31 21:27:03 -0500

    0.3.5

--- src/components/Modals.tsx
+++ src/components/Modals.tsx
@@ -1,4 +1,4 @@
-import { fmt, fmtMoney } from "../gameLogic";
+import { fmt, fmtMoney, RANDOM_SCENARIOS } from "../gameLogic";
 
 export function ReleaseModal({ state, doCloseReleasePresentation }: any) {
   const pres = state.releasePresentation;
